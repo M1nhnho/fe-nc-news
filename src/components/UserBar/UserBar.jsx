eac908dcfdce9bdd79d7491cb93b6db5deaba727
M1nhnho <tien.nguho@hotmail.com>
2024-03-18 01:02:17 +0000

    Added log in/out button.

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,15 +1,37 @@
 import './UserBar.css';
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UserContext } from '../../contexts/User.jsx';
 import UserTag from '../UserTag/UserTag.jsx';
 
 export default function UserBar()
 {
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
+    const [lastLoggedUser, setLastLoggedUser] = useState(null);
+
+    function logInOutUser()
+    {
+        user ? setUser(null) :
+        setUser({ username: "tickle122", name: "Tom Tickle", avatar_url: "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953" });
+    }
+
+    useEffect(() =>
+    {
+        if (user)
+        {
+            const isLastLoggedUser = lastLoggedUser && lastLoggedUser.username === user.username;
+            !isLastLoggedUser && setLastLoggedUser({...user});
+        }
+    }, [user]);
 
     return (
-        <div className='user-bar'>
-            <UserTag user={user} />
+        <div className="base-bar user-bar">
+            {lastLoggedUser && <UserTag user={lastLoggedUser} />}
+            <button 
+                aria-label="Log in or out the user tickle122"
+                id={user ? 'logout-button' : 'login-button'}
+                className={user ? 'button--red' : 'button--blue'}
+                onClick={logInOutUser}
+            ></button>
         </div>
     )
 }
