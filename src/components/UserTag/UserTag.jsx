130bd456ce4a799583dfe13b790236ab6bc11a14
M1nhnho <tien.nguho@hotmail.com>
2024-03-19 18:10:53 +0000

    Boldened logged in user everywhere.

--- src/components/UserTag/UserTag.jsx
+++ src/components/UserTag/UserTag.jsx
@@ -1,11 +1,19 @@
 import './UserTag.css';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/User.jsx';
 
 export default function UserTag({ user })
 {
+    const { user: loggedInUser } = useContext(UserContext);
+    const userIsLoggedIn = loggedInUser && loggedInUser.username === user.username;
+
     return (
         <div className="user-tag">
             <img alt={`${user.username}'s profile avatar`} src={user.avatar_url} />
-            <span className="user-tag__username">{user.username}</span>
+            <span
+                className={`user-tag__username ${userIsLoggedIn ? 'user-tag__username--logged-in' : ''}`}
+                username={user.username}
+            >{user.username}</span>
             <span className="user-tag__name">&nbsp;({user.name})</span>
         </div>
     )
